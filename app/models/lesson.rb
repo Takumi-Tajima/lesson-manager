6f04baf918a5e9a9ef56e85d522f1e2486d5f977
taji <taji@sonicgarden.jp>
2024-10-10 16:51:30 +0900

    feat: lesson.rbにvalidatesを追加

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,2 +1,8 @@
 class Lesson < ApplicationRecord
+  validates :name, presence: true
+  validates :name, :instructor, length: { maximum: 50 }
+  validates :overview, length: { maximum: 100 }
+  validates :hidden, inclusion: { in: [true, false] }
+
+  scope :default_order, -> { order(:created_at) }
 end
