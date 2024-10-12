774623d01edb820b8955d3aa5758b677674e413d
taji <taji@sonicgarden.jp>
2024-10-12 12:35:36 +0900

    fix: Userのnameに制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_11_082804) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
