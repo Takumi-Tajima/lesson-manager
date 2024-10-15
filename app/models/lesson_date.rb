d3eea69b2da5ea2ca1f8340a910794bb5165bb16
taji <taji@sonicgarden.jp>
2024-10-15 21:42:23 +0900

    feat: lesson_date.rbのpresenceにエラーメッセージを追加

--- app/models/lesson_date.rb
+++ app/models/lesson_date.rb
@@ -1,7 +1,7 @@
 class LessonDate < ApplicationRecord
   belongs_to :lesson
 
-  validates :date, :start_at, :end_at, :url, presence: true
+  validates :date, :start_at, :end_at, :url, presence: { message: '必須項目です' }
   validates :date, uniqueness: { scope: :lesson_id }, comparison: { greater_than: Date.current, message: 'レッスン日は、本日以降の日程を選択してください。' }
   validates :start_at, comparison: { greater_than: Time.current + 30.minutes, message: 'レッスンの開始時刻は、現時刻よりも30分以上間隔を開けて設定してください' }, if: :today?
   validates :end_at, comparison: { greater_than: :start_at, message: 'レッスンの終了時刻は、開始時刻よりも後に設定してください' }
