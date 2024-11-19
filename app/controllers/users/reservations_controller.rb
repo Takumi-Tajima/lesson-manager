5e6dada82d725e6fb510e3900e4c97b5fbdf5115
taji <taji@sonicgarden.jp>
2024-11-19 16:04:39 +0900

    予約一覧取得時にlessonテーブルごとpreloadする

--- app/controllers/users/reservations_controller.rb
+++ app/controllers/users/reservations_controller.rb
@@ -1,6 +1,6 @@
 class Users::ReservationsController < ApplicationController
   def index
-    @reservations = current_user.reservations.preload(lesson_date: :lesson.name).default_order.page(params[:page])
+    @reservations = current_user.reservations.preload(lesson_date: :lesson).default_order.page(params[:page])
   end
 
   def show
