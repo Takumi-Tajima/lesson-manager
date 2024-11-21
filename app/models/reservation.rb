7bf7bc86c737a7b182c0454e716fc8fd2efd2cc4
taji <taji@sonicgarden.jp>
2024-11-21 22:56:29 +0900

    日時が被っていた場合は、予約ができないバリデーションを追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -14,45 +14,29 @@ class Reservation < ApplicationRecord
     end
   end
 
-  # def same_date
-  #   user.lesson_dates.each do |booked_lesson_date|
-  #     next unless lesson_date.date == booked_lesson_date.date
-
-  #     errors.add(:base, '同じ日程です')
-  #   end
-  # end
-
-  # def overlapping
-  #   user.lesson_dates.each do |lesson_date_reserved|
-  #     next unless (lesson_date_reserved.start_at..lesson_date_reserved.end_at).overlap?(lesson_date.start_at..lesson_date.end_at)
-
-  #     errors.add(:base, 'すでに同じ時刻に予約をしています。')
-  #   end
-  # end
-
   def no_overlapping_reservations
     if dates_overlap?(user.lesson_dates) && times_overlap?(user.lesson_dates)
       errors.add(:base, 'すでに同じ時刻に予約をしています。')
     end
   end
 
-  def dates_overlap?(lesson_dates = nil)
-    return false unless lesson_dates
+  def dates_overlap?(lesson_dates)
+    return false if lesson_dates.empty?
 
     lesson_dates.each do |lesson_date_reserved|
       if lesson_date_reserved.date == lesson_date.date
         return true
       end
     end
+    false
   end
 
-  def times_overlap?(lesson_dates = nil)
-    return false unless lesson_dates
-
+  def times_overlap?(lesson_dates)
     lesson_dates.each do |lesson_date_reserved|
       if (lesson_date_reserved.start_at..lesson_date_reserved.end_at).overlap?(lesson_date.start_at..lesson_date.end_at)
         return true
       end
     end
+    false
   end
 end
