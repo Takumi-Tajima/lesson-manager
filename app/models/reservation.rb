3517f27ceef7948e42395cb0c161ab9480b59db1
taji <taji@sonicgarden.jp>
2024-11-21 22:19:39 +0900

    lesson時間被らないようにする（仮）

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,10 +6,51 @@ class Reservation < ApplicationRecord
 
   validates :user_id, uniqueness: { scope: :lesson_date_id, message: '同じレッスンを同日に2つ以上予約することはできません' }
   validate :limit_of_reservation
+  validate :no_overlapping_reservations
 
   def limit_of_reservation
     if lesson_date.capacity <= lesson_date.reservations.count
       errors.add(:base, 'このレッスンは満員です')
+    end
+  end
+
+  # def same_date
+  #   user.lesson_dates.each do |booked_lesson_date|
+  #     next unless lesson_date.date == booked_lesson_date.date
+
+  #     errors.add(:base, '同じ日程です')
+  #   end
+  # end
+
+  # def overlapping
+  #   user.lesson_dates.each do |lesson_date_reserved|
+  #     next unless (lesson_date_reserved.start_at..lesson_date_reserved.end_at).overlap?(lesson_date.start_at..lesson_date.end_at)
+
+  #     errors.add(:base, 'すでに同じ時刻に予約をしています。')
+  #   end
+  # end
+
+  def no_overlapping_reservations
+    if dates_overlap?(user.lesson_dates) && times_overlap?(user.lesson_dates)
+      errors.add(:base, 'すでに同じ時刻に予約をしています。')
+    end
+  end
+
+  def dates_overlap?(lesson_dates = nil)
+    return false unless lesson_dates
+
+    lesson_dates.each do |lesson_date_reserved|
+      if lesson_date_reserved.date == lesson_date.date
+        return true
+      end
+    end
+  end
+
+  def times_overlap?(lesson_dates)
+    lesson_dates.each do |lesson_date_reserved|
+      if (lesson_date_reserved.start_at..lesson_date_reserved.end_at).overlap?(lesson_date.start_at..lesson_date.end_at)
+        return true
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reservations, dependent: :destroy
+  has_many :lesson_dates, through: :reservations
 
   scope :default_order, -> { order(:id) }
 end
